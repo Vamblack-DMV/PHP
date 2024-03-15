4aa05992f6cd15de9156ad00ae1253c05ed7fd0a
Vamblack-DMV <vamblack2000@gmail.com>
2024-03-15 12:43:31 -0300

    first commit

---
+++ Aula2/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tipos no PHP - Aula 2</title>
+</head>
+<body>
+    <?php
+        // Variável
+        $var = "Pedro";
+        $Var = "Maria";
+        echo $var, " ", $Var, "</br>";
+        
+        // Array
+        $lista = array (10, 20, 22.20, "Pedro");
+        print_r ($lista);
+
+        // // Criando objetos
+        // $p1 = new Pessoa();
+        // $p1 -> Cadastro();
+        // $p1 -> nome = "Pedro";
+
+        // // Teoria em Prática
+        // $cliente = new Pessoa();
+        // $cliente -> nome;
+        // $cliente -> cpf;
+        // $cliente -> idade;
+        // $cliente -> endereco;
+
+        // $cliente -> CadastroCliente();
+
+        // $produto = new Estoque();
+        // $protudo -> camisa;
+        // $protudo -> cueca;
+        // $protudo -> blusa;
+
+        // $cliente = array("Pedro", 31231231231, 3199999999,"Rua Flor de Maio");
+        ?>
+</body>
+</html>
---
+++ Aula3/Index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Variáveis, Constatantes e Operadores - Aula 3</title>
+</head>
+<body>
+    <?php
+        // Variáveis
+        // $n1 = 12;
+        // $n1 = 25;
+        // $n1 = 12 + 13;
+        // $n1 = "12" + "13";
+        $n1 = "13 + 13";
+        echo $n1;
+
+        // Constantes
+        define ("DESCONTO", 10);
+        echo "</br>".DESCONTO;
+
+        const TAXA = 5;
+        echo "</br>".TAXA."</br>";
+
+        // Operadores lógicos
+        var_dump (true && true);
+        var_dump (false && true);
+        echo "</br>";
+        var_dump (true || true);
+        var_dump (false || true);
+        echo "</br>";
+        var_dump (!true);
+
+        // Teoria em Prática
+        echo 5 + 5, "</br>";
+        var_dump ( 2 + 3 );
+        echo "</br>";
+        var_dump ( 1 + 1.5);
+        echo "</br>";
+        echo 10 - 2, "</br>";
+        echo 10 * 2, "</br>";
+        echo 10 / 2, "</br>";
+        echo 10 % 2, "</br>";
+        echo 2 ** 3;
+    ?>
+</body>
+</html>
